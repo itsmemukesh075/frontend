0dc064d4cd3e2b084c09a238a62c5044daa7144f
itsmemukesh075 <itsmemukesh075@gmail.com>
2026-04-16 15:15:09 +0530

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,6 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
-
 export const metadata: Metadata = {
   title: "Contact Form App",
   description: "Simple form with Supabase",
